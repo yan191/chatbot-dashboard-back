3a1507bf61374de77e4c88aabf2fcf994ba034e2
Yan Rodrigues <yan.rodrigues.azevedo@hotmail.com>
2019-05-13 10:55:00 -0300

    change war pack

--- src/main/java/br/com/fatec/dashboard/DashboardApplication.java
+++ src/main/java/br/com/fatec/dashboard/DashboardApplication.java
@@ -2,19 +2,12 @@ package br.com.fatec.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DashboardApplication extends SpringBootServletInitializer {
+public class DashboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
 	}
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(DashboardApplication.class);
-    }
 
 }
