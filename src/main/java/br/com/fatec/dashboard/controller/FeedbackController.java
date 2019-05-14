289e13fb80b58a1d1ebba5ba3fc80e3f00634829
Yan Rodrigues <yan.rodrigues.azevedo@hotmail.com>
2019-05-14 14:27:30 -0300

    e

--- src/main/java/br/com/fatec/dashboard/controller/FeedbackController.java
+++ src/main/java/br/com/fatec/dashboard/controller/FeedbackController.java
@@ -1,7 +1,10 @@
 package br.com.fatec.dashboard.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,12 @@ public class FeedbackController {
 	public ResponseEntity<Feedback> createNewFeedback(@RequestBody Feedback feedback){
 		Feedback feed = feedbackService.save(feedback);
 		return ResponseEntity.ok().body(feed);
+	}
+	
+	@GetMapping("/feedbacks")
+	public ResponseEntity<List<Feedback>> getAllFeedback(){
+		List<Feedback> feedbacks = feedbackService.getAllFeedback();
+		return ResponseEntity.ok().body(feedbacks);
 	}
 
 }
--- src/main/java/br/com/fatec/dashboard/service/FeedbackService.java
+++ src/main/java/br/com/fatec/dashboard/service/FeedbackService.java
@@ -1,5 +1,7 @@
 package br.com.fatec.dashboard.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,10 @@ public class FeedbackService {
 		feed.setRating(feedback.getRating());
 		feedbackRepository.save(feed);
 		return feed;
+	}
+	
+	public List<Feedback> getAllFeedback() {
+		return feedbackRepository.findAll();
 	}
 
 }
